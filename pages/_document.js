91e22a7c65fad24ee25b8363ca1545ffb74f5f13
15217172098 <62952803+15217172098@users.noreply.github.com>
2023-09-02 01:11:46 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -11,8 +11,8 @@ class MyDocument extends Document {
     return (
       <Html lang={BLOG.lang}>
         <Head>
-          <link rel='icon' href='/ligo.png' />
-          <link rel='icon' href='/ligo.png' type='image/svg+xml' />
+          <link rel='icon' href='/favicon.ico' />
+          <link rel='icon' href='/favicon.svg' type='image/svg+xml' />
           {BLOG.appearance === 'auto' ? (
             <>
               <meta
